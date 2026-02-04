c260bab0cf9c7adcc357d0290a3de27da7aa2c25
JesusDiez <jesdiez@ucm.es>
2026-02-04 12:36:47 +0100

    mi primer comit

---
+++ SumarDosNumero/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SumarDosNumero
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Escribe el primer número: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Escribe el segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+
+            Console.Write("Escribe el segundo número: ");
+            int num3 = int.Parse(Console.ReadLine());
+
+            int resultado = num1 + num2  + num3;
+
+            Console.WriteLine("La suma es: " + resultado);
+            Console.ReadKey();
+        }
+    }
+}
